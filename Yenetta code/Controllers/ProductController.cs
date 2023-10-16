3b5fd0d8bdb9a6e626d90286f4f1fa80b912419c
gramville <buruk.girma.ve0887@gmail.com>
2023-10-16 19:55:38 +0300

    product controller and index page view

---
+++ Yenetta code/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Yenetta_code.Models.Services;
+
+namespace Yenetta_code.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly IBrandService _brandService;
+        private readonly ICategoryService _categoryService;
+        public ProductController(IProductService productService, IBrandService brandService, ICategoryService categoryService)
+        {
+            _productService = productService;
+            _brandService = brandService;
+            _categoryService = categoryService;
+
+        }
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productService.GetAll();
+            return View(products);
+        }
+    }
+}
