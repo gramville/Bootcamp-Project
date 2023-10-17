c4f5f8cdf97c817b0179049d55d1448147bc71ef
gramville <buruk.girma.ve0887@gmail.com>
2023-10-17 21:32:01 +0300

    added a response data transfer object(DTO) for the product model class

---
+++ Yenetta code/Models/DTOs/ResponseDTOs/ProductResponseDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace Yenetta_code.Models.DTOs.ResponseDTOs
+{
+    public class ProductResponseDTO
+    {
+        public int id { get; set; }
+        public string productName { get; set; }
+        public string slug { get; set; }
+        public string description { get; set; }
+        public float price { get; set; }
+        public int quantity { get; set; }
+        public string brandName { get; set; }
+        public string categoryName { get; set; }
+        public bool isDeleted { get; set; } = false;
+    }
+}
